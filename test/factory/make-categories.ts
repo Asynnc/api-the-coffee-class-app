89198c597c86a8e462ce4023ce8205778eaabed7
Antonio Silva <antoniobsilvaprof@hotmail.com>
2023-06-04 15:39:35 -0300

    feat: add factory make-category.ts

--- test/factory/make-categories.ts
+++ test/factory/make-categories.ts
@@ -2,15 +2,15 @@ import { faker } from '@faker-js/faker';
 import { Category, CategoryProps } from '../../source/domain/orders/enterprises/entities/category';
 import { UniqueEntityID } from '../../source/core/entities/unique-entity-id';
 
-export function MakeQuestion(
+export function MakeCategory(
   override: Partial<CategoryProps> = {},
   id?: UniqueEntityID
 ) {
-  const question = Category.create({
+  const category = Category.create({
     name: faker.commerce.department(),
     icon: faker.internet.emoji({ types: ['food'] }),
     ...override
   }, id);
 
-  return question;
+  return category;
 }
